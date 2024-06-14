ff80a523c0eee43a853f75595695fce0ad33c91d
Dave G <dpg3k@virginia.edu>
2024-06-14 07:49:29 -0400

    Correct struct fields

--- lambda-common/main-lambda-sqs.go
+++ lambda-common/main-lambda-sqs.go
@@ -9,7 +9,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -19,21 +18,10 @@ func HandleRequest(ctx context.Context, event events.S3Event) error {
 
 	var returnErr error
 
-	// loop through possible messages
-	for _, message := range event.Records {
-		// convert to an S3 event
-		var s3Event events.S3EventRecord
-		err := json.Unmarshal([]byte(message.Body), &s3Event)
-		if err != nil {
-			fmt.Printf("ERROR: unmarshaling S3 event (%s), continuing\n", err.Error())
-			returnErr = err
-			continue
-		}
-
+	// loop through possible event records
+	for _, eventRecord := range event.Records {
 		// process the message, in the event of an error, it is re-queued
-		bucket := s3Event.S3.Bucket.Name
-		key := s3Event.S3.Object.Key
-		err = process(s3Event.Source, bucket, key)
+		err := process(eventRecord.EventSource, eventRecord.S3.Bucket.Name, eventRecord.S3.Object.Key)
 		if err != nil {
 			fmt.Printf("ERROR: processing S3 event (%s), continuing\n", err.Error())
 			returnErr = err
