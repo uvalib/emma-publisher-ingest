657283a632715b4fd3830a3f77ed663669a8250e
Dave G <dpg3k@virginia.edu>
2024-06-17 10:02:41 -0400

    Actually download the file

--- bibliovault-ingest/process.py
+++ bibliovault-ingest/process.py
@@ -2,11 +2,20 @@
 #
 #
 
+import boto3
+
 def process(bucket, key):
 
 	print( "EVENT: " + bucket + "/" + key + "\n" )
 
-        # do the processing here...
+	s3 = boto3.resource('s3')
+
+        # download the file
+	print("DEBUG: downloading s3://" + bucket + "/" + key + ":\n")
+	response = s3.Object(bucket, key).get()
+	buf = response['Body'].read()
+
+        # do the processing
 
 	# all is well
 	return None
