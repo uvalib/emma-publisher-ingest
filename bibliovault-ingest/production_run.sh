30a07cccc34a76698556abcda0758210db033fd5
Robert Haschart <rh9ec@virginia.edu>
2024-07-23 12:14:44 -0400

    updated Makefile and Pipfile and .gitignore, added smal scripts containing definitions for running in production or staging

---
+++ bibliovault-ingest/production_run.sh
@@ -0,0 +1,9 @@
+. secret_keys.sh
+export EMMA_INGESTION_LIMIT=50
+export OPENSEARCH_URL=localhost:9200
+export OPENSEARCH_INDEX=emma-federated-index-production
+export BASTION_HOST=3.237.97.223
+export BASTION_USERNAME=ec2-user
+export BASTION_REMOTE_HOST=vpc-emma-index-production-glc53yq4angokfgqxlmzalupqe.us-east-1.es.amazonaws.com
+export BASTION_SSHKEY=bastion_production.pem
+
