16f0ba863d55548ad8dd2329b078d3c4a480b7f6
Robert Haschart <rh9ec@virginia.edu>
2024-07-29 13:27:39 -0400

    separate scripts for running with tunnel vs running directly

---
+++ bibliovault-ingest/production_run_local.sh
@@ -0,0 +1,7 @@
+if [[ -f secret_keys.sh ]] ; then
+    . secret_keys.sh
+fi
+export EMMA_INGESTION_LIMIT=200
+export OPENSEARCH_URL=vpc-emma-index-production-glc53yq4angokfgqxlmzalupqe.us-east-1.es.amazonaws.com
+export OPENSEARCH_INDEX=emma-federated-index-production
+export EMMA_STATUS_TABLE_NAME=emma_bookshare_loader_production
---
+++ openalex-ingest/staging_run_local.sh
@@ -0,0 +1,9 @@
+if [[ -f secret_keys.sh ]] ; then
+    . secret_keys.sh
+fi
+export EMMA_INGESTION_LIMIT=200
+export OPENSEARCH_URL=vpc-emma-nonlive-ip5v3sq3r2im6pvyvjcadlgzvm.us-east-1.es.amazonaws.com
+export OPENSEARCH_INDEX=emma-federated-index-staging-1.0.1
+export EMMA_STATUS_TABLE_NAME=emma_bookshare_loader_staging
+
+
