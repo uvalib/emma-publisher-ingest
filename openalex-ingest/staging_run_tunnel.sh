41f14ed29401d3ee9f114791830fab1735d7040b
Robert Haschart <rh9ec@virginia.edu>
2024-08-14 17:09:34 -0400

    change bastion host IP address, make changes to openalex handler similar to those made for internet_archive handler

--- openalex-ingest/staging_run_tunnel.sh
+++ openalex-ingest/staging_run_tunnel.sh
@@ -4,7 +4,7 @@ fi
 export EMMA_INGESTION_LIMIT=200
 export OPENSEARCH_URL=localhost:9200
 export OPENSEARCH_INDEX=emma-federated-index-staging-1.0.1
-export BASTION_HOST=54.236.31.64
+export BASTION_HOST=3.235.161.93
 export BASTION_USERNAME=ec2-user
 export BASTION_REMOTE_HOST=vpc-emma-nonlive-ip5v3sq3r2im6pvyvjcadlgzvm.us-east-1.es.amazonaws.com
 export BASTION_SSHKEY=bastion_staging.pem
