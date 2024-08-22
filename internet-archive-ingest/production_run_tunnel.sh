ce97302d70d694d04a5175bd2152e2f2b3e00bbc
Robert Haschart <rh9ec@virginia.edu>
2024-08-22 13:56:38 -0400

    fixing the bastion IP for production

--- internet-archive-ingest/production_run_tunnel.sh
+++ internet-archive-ingest/production_run_tunnel.sh
@@ -4,7 +4,7 @@ fi
 export EMMA_INGESTION_LIMIT=500
 export OPENSEARCH_URL=localhost:9200
 export OPENSEARCH_INDEX=emma-federated-index-production
-export BASTION_HOST=3.237.97.223
+export BASTION_HOST=18.213.246.145
 export BASTION_USERNAME=ec2-user
 export BASTION_REMOTE_HOST=vpc-emma-index-production-glc53yq4angokfgqxlmzalupqe.us-east-1.es.amazonaws.com
 export BASTION_SSHKEY=bastion_production.pem
